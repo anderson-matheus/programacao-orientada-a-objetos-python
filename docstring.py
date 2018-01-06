b735c40c99ebf02bd6e022b5bc8d557586d0a9b6
anderson-matheus <matheus5347@gmail.com>
2018-01-06 14:43:26 -0200

    Módulos e pacotes, fila de prioridades e docstring

---
+++ docstring.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+def multiplicar(x, y):
+    """Essa função retorna a multiplicação de dois números."""
+    return x * y
+
+
+help(multiplicar)
+print(multiplicar.__doc__)
+
+
+class MinhaClasse:
+    """Classe utilizada para representar algo"""
+    pass
+
+
+help(MinhaClasse)
---
+++ modulos-e-pacotes.py
@@ -0,0 +1,9 @@
+# from poligono import Quadrado
+from modulo.poligono import Quadrado, Retangulo
+
+q = Quadrado(4)
+print(q.perimetro())
+print(q.area())
+
+r = Retangulo(5, 10)
+print(r.area())
